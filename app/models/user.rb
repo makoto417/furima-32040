a80f349b5b5a99329fd3e57da0de33763c2c2bf6
makoto <makoto@users.noreply.github.com>
2020-11-14 14:28:34 +0900

    usersテーブルの作成、バリデーションの記述

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, null: false
+  validates :family_name, null: false
+  validates :first_name, null: false
+  validates :family_name_kana, null: false
+  validates :first_name_kana, null: false
+  validates :birthday, null: false
+
+end
