f72904edf99a8f6909dc9105e0dea01c0119f8ca
makoto <makoto@users.noreply.github.com>
2020-11-14 16:03:46 +0900

    新規登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, null: false
-  validates :family_name, null: false
-  validates :first_name, null: false
-  validates :family_name_kana, null: false
-  validates :first_name_kana, null: false
-  validates :birthday, null: false
+  
+  validates :nickname, presence: true
+  validates :password, format: {with: /\A[a-z0-9]+\z/i}
+  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday, presence: true
 
 end
